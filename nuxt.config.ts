dec7ba6e47ec10aafc6c10b676376f16de44b623
Tanay Karnik <tanayvk@gmail.com>
2024-10-03 19:14:36 +0530

    feat: add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   // devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/fonts"],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "nuxt-gtag"],
   fonts: {
     families: [{ name: "Fredoka", provider: "google" }],
+  },
+  gtag: {
+    id: "G-VH3Y7P4PLH",
   },
 });
