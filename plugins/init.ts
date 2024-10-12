ff35966bdb87cfa41a9bf6b39dc5740562232ae1
Tanay Karnik <tanayvk@gmail.com>
2024-10-12 21:20:13 +0530

    fix: attempt fix issue with app loading on mobile

--- plugins/init.ts
+++ plugins/init.ts
@@ -1,13 +1,12 @@
 export default defineNuxtPlugin({
   hooks: {
     async "app:beforeMount"() {
-      useName().value = localStorage.getItem("peersplit.name") || "New User";
-      await dbInit();
-      await updateGroups();
-      initGun();
-      // init main database
-      // init all group databases
-      // start sync for all groups
+      try {
+        useName().value = localStorage.getItem("peersplit.name") || "New User";
+        await dbInit();
+        await updateGroups();
+        initGun();
+      } catch {}
     },
   },
 });
